1bb79903c5f1a459d559ec204451bec5a6a75f8b
Klaus Etscheidt <klaus.etscheidt@gmail.com>
2024-02-07 20:16:39 +0100

    xx

--- doku_help/source/conf.py
+++ doku_help/source/conf.py
@@ -26,7 +26,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon', 'myst_parser']
 
 templates_path = ['_templates']
 exclude_patterns = []
